bab00e3b514e060ce9d1a66452ea49258962338e
btb4018 <79254020+btb4018@users.noreply.github.com>
2021-03-25 15:22:25 +0000

    Update Keypad.s

--- Keypad.s
+++ Keypad.s
@@ -1,6 +1,6 @@
     #include<xc.inc>
 
-global	Keypad, keypad_val, keypad_ascii
+global	Keypad, keypad_val
 
 extrn	LCD_delay_ms
     
@@ -9,7 +9,6 @@ col_input:	ds 1	; reserve 1 byte for variable Col_input
 row_input:  ds 1
 keypad_input: ds 1
 delay_val: ds 1
-keypad_ascii: ds 1
 keypad_val: ds 1
  
 psect	keypad_code,class=CODE	
@@ -17,19 +16,19 @@ psect	keypad_code,class=CODE
 Keypad:
 	call Keypad_Setup
 	
-	    ;trigger and read column input;
+	;trigger and read column input;
 	movlw	0x0f
 	movwf	TRISE	;set E0-E3 as input and E4-E7 as output
-	call delay_keypad
+	call 	delay_keypad
 	movff	PORTE, col_input    ;store column input in col_input
 	
-	    ;trigger and read row input;
+	;trigger and read row input;
 	movlw	0xf0
 	movwf	TRISE	;set E4-7 as input, E0-E3 as output
 	call	delay_keypad
 	movff	PORTE, row_input    ;store row input in row_input
 	
-	    ;add row and column input;
+	;add row and column input;
 	movf	row_input, W, A
 	addwf	col_input, 0, 0  ; add row and column bytes and store result in 0x24
 	movwf	keypad_input	; check if added value is correct by reading onto PORTH
@@ -58,8 +57,6 @@ check_null:
 	bra check1
 	movlw	0xff
 	movwf	keypad_val, A	
-	movlw	0x00	;ascii for null
-	movwf	keypad_ascii, A
 	return
 	
 check1:	movlw	11101110B	;move keypad value expected from 1 button into W
@@ -67,8 +64,6 @@ check1:	movlw	11101110B	;move keypad value expected from 1 button into W
 	bra check2		;branch to next check if not equal
 	movlw	0x01		;if equal set keypad_val to 0x01
 	movwf	keypad_val	
-	movlw	0x31		;ascii	code for 1
-	movwf	keypad_ascii	;set keypad_ascii to 1 ascii code
 	return
 	
 check2:	movlw	11101101B
@@ -76,8 +71,6 @@ check2:	movlw	11101101B
 	bra	check3
 	movlw	0x02	
 	movwf	keypad_val, A
-	movlw	0x32		;ascii for 2
-	movwf	keypad_ascii
 	return
 	
 check3:	movlw	11101011B  
@@ -85,8 +78,6 @@ check3:	movlw	11101011B
 	bra checkF
 	movlw	0x03
 	movwf	keypad_val, A
-	movlw	0x33		;aascii for 3
-	movwf	keypad_ascii
 	return
  
 checkF:	movlw	11100111B
@@ -94,8 +85,6 @@ checkF:	movlw	11100111B
 	bra check4
 	movlw	0x0F
 	movwf	keypad_val, A
-	movlw	0x46		;ascii for F
-	movwf	keypad_ascii
 	return
 	
 check4:	movlw	11011110B   
@@ -103,8 +92,6 @@ check4:	movlw	11011110B
 	bra check5
 	movlw	0x04	
 	movwf	keypad_val, A
-	movlw	0x34		;ascii for 4
-	movwf	keypad_ascii
 	return
  
 check5:	movlw	11011101B
@@ -112,8 +99,6 @@ check5:	movlw	11011101B
 	bra check6
 	movlw	0x05	
 	movwf	keypad_val, A
-	movlw	0x35		;ascii for 5
-	movwf	keypad_ascii
 	return
 	
 check6:	movlw	11011011B  
@@ -121,8 +106,6 @@ check6:	movlw	11011011B
 	bra checkE
 	movlw	0x06	
 	movwf	keypad_val, A
-	movlw	0x36		;ascii for 6
-	movwf	keypad_ascii
 	return
  
 checkE:	movlw	11010111B
@@ -130,8 +113,6 @@ checkE:	movlw	11010111B
 	bra check7
 	movlw	0x0E	
 	movwf	keypad_val, A
-	movlw	0x45		;ascii for E
-	movwf	keypad_ascii
 	return
 	
 check7:	movlw	10111110B 
@@ -139,8 +120,6 @@ check7:	movlw	10111110B
 	bra check8	
 	movlw	0x07	
 	movwf	keypad_val, A
-	movlw	0x37		;ascii for 7
-	movwf	keypad_ascii
 	return
  
 check8:	movlw	10111101B
@@ -148,8 +127,6 @@ check8:	movlw	10111101B
 	bra check9
 	movlw	0x08	
 	movwf	keypad_val, A
-	movlw	0x38		;ascii for 8
-	movwf	keypad_ascii
 	return
 	
 check9:	movlw	10111011B   
@@ -157,8 +134,6 @@ check9:	movlw	10111011B
 	bra checkD
 	movlw	0x09   
 	movwf	keypad_val, A
-	movlw	0x39		;ascii for 9 
-	movwf	keypad_ascii
 	return
 	
 checkD:	movlw	10110111B  
@@ -166,8 +141,6 @@ checkD:	movlw	10110111B
 	bra checkA
 	movlw	0x0D	
 	movwf	keypad_val, A
-	movlw	0x44		;ascii for D
-	movwf	keypad_ascii
 	return
  
 checkA:	movlw	01111110B
@@ -175,8 +148,6 @@ checkA:	movlw	01111110B
 	bra check0
 	movlw	0x0A	
 	movwf	keypad_val, A
-	movlw	0x41		;ascii for A
-	movwf	keypad_ascii
 	return
 	
 check0:	movlw	01111101B  
@@ -184,8 +155,6 @@ check0:	movlw	01111101B
 	bra checkB
 	movlw	0x00	
 	movwf	keypad_val, A
-	movlw	0x30		;ascii for 0
-	movwf	keypad_ascii
 	return
  
 checkB: movlw	01111011B
@@ -193,8 +162,6 @@ checkB: movlw	01111011B
 	bra checkC
 	movlw	0x0B	
 	movwf	keypad_val, A
-	movlw	0x42		;ascii for B
-	movwf	keypad_ascii
 	return
 	
 checkC:	movlw	01110111B   
@@ -202,13 +169,9 @@ checkC:	movlw	01110111B
 	bra null
 	movlw	0x0C	
 	movwf	keypad_val, A
-	movlw	0x43		;ascii for C
-	movwf	keypad_ascii
 	return
 	
-null:	movlw	0x00
-	movwf	keypad_ascii
-	movlw	0xff
+null:	movlw	0xff
 	movwf	keypad_val
 	return
 	
@@ -216,8 +179,6 @@ null:	movlw	0x00
 delay_keypad:	
 	movlw	0x40
 	call	LCD_delay_ms
-	;decfsz	delay_val
-	;bra delay_keypad
 	return
 
 	end 
