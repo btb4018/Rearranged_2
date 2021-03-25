628ae1691457af60af9c60467a042fb5862e9cca
btb4018 <79254020+btb4018@users.noreply.github.com>
2021-03-25 15:31:38 +0000

    Update Alarm.s

--- Alarm.s
+++ Alarm.s
@@ -1,7 +1,9 @@
 #include <xc.inc>
     
 extrn	LCD_Set_Position 
-    
+extrn	Keypad, keypad_val
+extrn	hex_A, hex_B, hex_C, hex_D, hex_E, hex_F, hex_null
+extrn	Write_ALARM    
     
 global	alarm_on
 global	alarm_sec, alarm_min, alarm_hrs 
@@ -14,14 +16,12 @@ alarm_hrs:	ds  1
     
 alarm_on:	ds  1
     
-buzz_bit: ds	1
+buzz_on_or_off: ds	1
     
 buzzer_counter_1: ds 1
 buzzer_counter_2: ds 1
     
-alarm_buzz: ds 1    
-    
-skip_byte: ds 1
+alarm_countdown: ds 1    
 
 psect	Alarm_code, class=CODE
     
@@ -32,25 +32,25 @@ Alarm_Setup:
 	movwf	alarm_hrs, A
 	
 	bcf	alarm_on, 0, A
-	bcf	buzz_bit, 0, A  
+	bcf	buzz_on_or_off, 0, A  
 	
-	clrf	Alarm_buzz, A
+	clrf	alarm_countdown, A
 	
 	return
     
 Check_Alarm:
 	movlw	0x00
-	cpfseq	Alarm_buzz		;check if alarm_buzz has reached 0
-	bra	decrement_alarm_buzz	;keep decrementing and buzzing if it hasn't
+	cpfseq	alarm_countdown		;check if alarm_buzz has reached 0
+	bra	decrement_alarm_countdown	;keep decrementing and buzzing if it hasn't
 	bra	compare_alarm		;go to compare alarm and return to normal cycles if it has reached 0
 
 decrement_alarm_buzz:
-	decf	Alarm_buzz		;subroutine to keep decrementing alarm_buzz
+	decf	alarm_countdown		;subroutine to keep decrementing alarm_buzz
 	call	ALARM			;and buzzing
 	return
 	
 ;try instead of line 120-129?
-;	decfsz	alarm_buzz
+;	decfsz	alarm_countdown
 ;	BNN	ALARM
 ;	BRA	Compare_Alarm
 	
@@ -68,31 +68,30 @@ Compare_Alarm:				;compare alarm
 	return			    ;return if not the same
 	
 	movlw	0x3C
-	movwf	alarm_buzz, A	    ;set alarm_buzz to 60 to be able to buzz for 60 seconds
+	movwf	alarm_countdown, A	    ;set alarm_buzz to 60 to be able to buzz for 60 seconds
 	    
 	call	ALARM		    ;call alarm if it is the same
 	return
 ALARM:
-	movlw	11000000B	    ;set cursor to first line
-	call	LCD_Set_Position
-	call	Display_ALARM	    ;display alarm while alarm is ringing
+	call	LCD_Set_to_Line_1
+	call	Write_ALARM	    ;display alarm while alarm is ringing
 	
 	call	check_buzz_bit	    ;check the buzz_bit to set it if it was clear and to clear it if it was set
-	;BTG	buzz_bit, 0
+	;BTG	buzz_on_or_off, 0
 	
 	call	Buzzer		    ;call buzzer which buzzes when the buzz_bit is set
 
 	return	
 		
-check_buzz_bit:
-	btfsc	buzz_bit, 0, A			;check if buzz bit is set
+check_buzz_on_or_off:
+	btfsc	buzz_on_or_off, 0, A			;check if buzz bit is set
 	bra	clear_buzz_bit		;branch to set it if it isnt set
-	bra	set_buzz_bit		;branch to clear it if it is set
-clear_buzz_bit:	
-	bcf	buzz_bit, 0, A			;clear buzz_bit 
+	bra	set_buzz_on_or_off		;branch to clear it if it is set
+clear_buzz_on_or_off:	
+	bcf	buzz_on_or_off, 0, A			;clear buzz_bit 
 	return
-set_buzz_bit:
-	bsf	buzz_bit, 0, A			;set buzz_bit
+set_buzz_on_or_off:
+	bsf	buzz_on_or_off, 0, A			;set buzz_bit
 	return
 	
 Buzzer:	
@@ -130,7 +129,7 @@ Buzz_Loop_2:
 
 Buzz_sequence:	
 Check_if_Buzz:
-	btfss	buzz_bit, 0, A	
+	btfss	buzz_on_or_off, 0, A	
 	bra	No_Buzz
 	bra	Yes_Buzz
 No_Buzz: 
@@ -150,11 +149,11 @@ Delay_Buzzer:
 	return
 
 Cancel_Alarm:
-	clrf	alarm_buzz, A	
+	clrf	alarm_countdown, A	
 	return
 	
 Snooze_Alarm:
-	clrf    alarm_buzz, A	
+	clrf    alarm_countdown, A	
 	call	Write_Snooze
 	movlw	0x05
 	addwf	alarm_min, A	
